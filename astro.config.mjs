192129b5b8313d99baa9119ff33b85223453d655
Edwin Rivera <gaedwin92@gmail.com>
2026-05-20 13:47:51 -0600

    Fix: deploy v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import sitemap from '@astrojs/sitemap';
-import { blogKit } from "astro-blog-kit/integration";
 
 const SITE = process.env.SITE || "https://roedwin-afk.github.io";
 const BASE = process.env.BASE || "/landing_purandpirinsulation/";
@@ -9,7 +8,6 @@ export default defineConfig({
   site: SITE,
   base: BASE,
   output: "static",
-  integrations: [sitemap()
-  ],
+  integrations: [sitemap()],
 });
 
